68c7d13942b04881c6d172d6c53f82c0982988f5
ualvesdias <ulisses.alves@protonmail.com>
2019-03-21 23:48:19 -0300

    Create Board.py

---
+++ Board.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+class Board(object):
+	"""Loads and updates the boardgame"""
+	def __init__(self):
+		super(Board, self).__init__()
+		self.board = np.full((3,3), 0)
+		self.board[0,0] = '2'
+		self.board[0,1] = '2'
+		self.board[0,2] = '2'
+		self.board[2,0] = '1'
+		self.board[2,1] = '1'
+		self.board[2,2] = '1'
+		
+	def convertMove(self, move):
+		try:
+			frto = move.split(':')
+			fr = list(map(int, frto[0].split(',')))
+			to = list(map(int, frto[1].split(',')))
+		except:
+			return False
+		return fr, to
+
+	def updateBoard(self, fr, to):
+		self.board[to[0],to[1]] = self.board[fr[0],fr[1]]
+		self.board[fr[0],fr[1]] = 0
+
+	def isMoveValid(self, fr, to, player):
+		if player == 0:
+			print('0')
+			return (((fr[0] - to[0] == 1 and fr[1] == to[1]) and self.board[to[0],to[1]] == 0) or ((fr[0] - to[0] == 1 and abs(fr[1] - to[1]) == 1) and (self.board[to[0],to[1]] != 0 and self.board[to[0],to[1]] != self.board[fr[0],fr[1]])))
+		else:
+			print('1')
+			return (((fr[0] - to[0] == -1 and fr[1] == to[1]) and self.board[to[0],to[1]] == 0) or ((fr[0] - to[0] == -1 and abs(fr[1] - to[1]) == 1) and (self.board[to[0],to[1]] != 0 and self.board[to[0],to[1]] != self.board[fr[0],fr[1]])))
+
+	def printBoard(self):
+		print('    0   1   2  ')
+		for i in range(3):
+			print('%i | %s | %s | %s |' % (i,self.board[i,0],self.board[i,1],self.board[i,2]))
+
+	def getBoardState(self):
+		self.state = ''
+		for i in range(3):
+			for j in range(3):
+				self.state += str(self.board[i,j])
+		return self.state
